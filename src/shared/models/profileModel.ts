52be449ec2b2250bfa2d876c433e009311dca531
Hasitha Lakshan <44424716+Hasitha-Lakshan@users.noreply.github.com>
2023-04-13 16:38:12 +0530

    Added functions to get collection center details

--- src/shared/models/profileModel.ts
+++ src/shared/models/profileModel.ts
@@ -4,24 +4,41 @@ export interface ProfileRequest {
 }
 
 export interface CollectionCenterSettingsResponse {
-    username: string
-    centerName: string
-    contactNumber: number
-    email: string
-    addressLine1: string
-    addressLine2: string
-    addressLine3: string
-    location: string
-    active: boolean
+    username: string;
+    centerName: string;
+    contactNumber: number;
+    email: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    location: string;
+    active: boolean;
 }
 
 export interface UserSettingsResponse {
-    username: string
-    firstName: string
-    lastName: string
-    contactNumber: number
-    email: string
-    addressLine1: string
-    addressLine2: string
-    addressLine3: string
+    username: string;
+    firstName: string;
+    lastName: string;
+    contactNumber: number;
+    email: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+}
+
+export interface CollectionCenterDetailsResponse {
+    username: string;
+    centerName: string;
+    contactNumber: number;
+    email: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    location: string;
+    description: string;
+    wasteType: string;
+    payment: number;
+    active: boolean;
+    accountStatus: boolean;
+    workingDays: string[];
 }
